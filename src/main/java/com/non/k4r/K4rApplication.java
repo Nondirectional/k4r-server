86dba26e2e2630b71684e606cc184407e6363e4d
Nondirectional <nondirectional@yeesun.com>
2024-08-01 14:24:53 +0800

    Initial commit

---
+++ src/main/java/com/non/k4r/K4rApplication.java
@@ -0,0 +1,13 @@
+package com.non.k4r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K4rApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K4rApplication.class, args);
+    }
+
+}
