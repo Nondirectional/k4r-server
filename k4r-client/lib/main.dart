9fd53634153aaba0ee352e08f540aed444e44cd3
Nondirectional <nondirectional@yeesun.com>
2024-08-07 13:42:52 +0800

    简单的注册页

--- k4r-client/lib/main.dart
+++ k4r-client/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:k4r_client/pages/home_page.dart';
-import 'package:k4r_client/pages/login_page.dart';
+import 'package:k4r_client/pages/sign_in_page.dart';
+import 'package:k4r_client/pages/sign_up_page.dart';
 import 'package:k4r_client/providers/access_token_provider.dart';
 import 'package:k4r_client/providers/logged_sate_provider.dart';
 import 'package:provider/provider.dart';
@@ -22,17 +23,20 @@ void main() {
           routes: [
             GoRoute(path: "/", builder: (context, state) => const HomePage()),
             GoRoute(
-              path: '/login',
-              builder: (context, state) => LoginPage(),
-            )
+              path: '/sign-in',
+              builder: (context, state) => SignInPage(),
+            ),
+            GoRoute(
+                path: "/sign-up",
+                builder: (context, state) => const SignUpPage()),
           ],
           redirect: (context, state) {
             final LoggedSateProvider loggedState =
             Provider.of<LoggedSateProvider>(context,listen: false);
-            final bool inLoginPage = state.uri.path == '/login';
-            if (!loggedState.isLoggedIn && !inLoginPage) {
+            Set<Uri> whiteList = {Uri.parse('/sign-in'), Uri.parse('/sign-up')};
+            if (!loggedState.isLoggedIn && !whiteList.contains(state.uri)) {
               print("current user has not logged,redirect to login page.");
-              return '/login';
+              return '/sign-in';
             }
           }),
     ),
---
+++ k4r-client/lib/pages/sign_in_page.dart
@@ -0,0 +1,112 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:k4r_client/component/api_caller.dart';
+import 'package:k4r_client/providers/logged_sate_provider.dart';
+import 'package:provider/provider.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  SignInPageState createState() => SignInPageState();
+}
+
+class SignInPageState extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _username;
+  late String _password;
+  bool loggedIn = false;
+
+  void _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState?.save();
+      ApiCaller apiCaller = ApiCaller();
+      Response? response = await apiCaller.signin(_username, _password, false);
+      if (response != null) {
+        print(response.data);
+        setState(() {
+          loggedIn = true;
+          LoggedSateProvider loggedSate =
+          Provider.of<LoggedSateProvider>(context, listen: false);
+          loggedSate.login();
+          GoRouter.of(context).go('/');
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('登录'),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 22,horizontal: 16.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: '用户名'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return '请输入用户名';
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (value) =>
+                      FocusScope.of(context).nextFocus(),
+                  onSaved: (value) => _username = value!,
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 22,horizontal: 16.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: '密码'),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return '请输入密码';
+                      }
+                      return null;
+                    },
+                    onFieldSubmitted: (value) =>
+                        FocusScope.of(context).nextFocus(),
+                    onSaved: (value) => _password = value!,
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 26.0,vertical: 22.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 36.0),
+                      color: Colors.black38,
+                      onPressed: () {
+                        GoRouter.of(context).go('/sign-up');
+                      },
+                      child: const Text('注册'),
+                    ),
+                    MaterialButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 36.0),
+                      color: Colors.black38,
+                      onPressed: _submitForm,
+                      child: const Text('登录'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
